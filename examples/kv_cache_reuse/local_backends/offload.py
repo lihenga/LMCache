c9e7c87068c5ef5d3d6885a2449edb88e328350f
lihenga <1438677126@qq.com>
2026-06-29 14:33:45 +0000

    test

--- examples/kv_cache_reuse/local_backends/offload.py
+++ examples/kv_cache_reuse/local_backends/offload.py
@@ -61,8 +61,9 @@ def build_llm_with_lmcache(lmcache_connector: str, model: str, vllm_version: str
     llm_kwargs = {
         "model": model,
         "kv_transfer_config": ktc,
-        "max_model_len": 8000,
+        "max_model_len": 4096,
         "gpu_memory_utilization": 0.8,
+        "enforce_eager": True,
     }
     if vllm_version == "v0":
         llm_kwargs["enable_chunked_prefill"] = True  # Only in v0
@@ -119,17 +120,17 @@ def main():
 
     if args.version == "v0":
         lmcache_connector = "LMCacheConnector"
-        model = "mistralai/Mistral-7B-Instruct-v0.2"
+        model = "Qwen/Qwen3-0.6B"
     else:
         lmcache_connector = "LMCacheConnectorV1"
-        model = "mistralai/Mistral-7B-Instruct-v0.2"
+        model = "Qwen/Qwen3-0.6B"
 
     setup_environment_variables(args.version, args.use_disk)
 
     with build_llm_with_lmcache(lmcache_connector, model, args.version) as llm:
         # This example script runs two requests with a shared prefix.
         # Define the shared prompt and specific prompts
-        shared_prompt = "Hello, how are you?" * 1000
+        shared_prompt = "Hello, how are you?" * 300
         first_prompt = [
             shared_prompt + "Hello, my name is",
         ]
